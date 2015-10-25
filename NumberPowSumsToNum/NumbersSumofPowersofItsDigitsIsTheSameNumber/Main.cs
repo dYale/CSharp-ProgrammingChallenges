15e0ad628d5dc598beba7ad6cbf36a371407699d
Dan Corman <dan.corman@gmail.com>
2015-10-25 15:53:04 -0500

    adding NumbersSumToPowersOfItsDigitsIsTheSame toy problem

---
+++ NumberPowSumsToNum/NumbersSumofPowersofItsDigitsIsTheSameNumber/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NumbersSumofPowersofItsDigitsIsTheSameNumber
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
